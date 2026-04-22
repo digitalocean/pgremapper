9afb68a0c4a8ad547c5fe1036a013158cced80bd
Sean Jenkins <seanjnkns@gmail.com>
2026-04-22 13:41:09 -0600

    Update to go 1.26.2 along with modules and fixes

--- main.go
+++ main.go
@@ -337,8 +337,12 @@ mapping), unless --whole-pg is specified.
 				if err != nil {
 					panic(err)
 				}
-				defer f.Close()
 
+				defer func() {
+					if err := f.Close(); err != nil {
+						panic(err)
+					}
+				}()
 				writer = f
 			}
 
@@ -400,7 +404,11 @@ cluster undergoes some other major changes).
 				if err != nil {
 					panic(err)
 				}
-				defer f.Close()
+				defer func() {
+					if err := f.Close(); err != nil {
+						panic(err)
+					}
+				}()
 
 				writer = f
 			}
@@ -452,7 +460,12 @@ JSON format example, remapping PG 1.1 from OSD 100 to OSD 42:
 				if err != nil {
 					panic(err)
 				}
-				defer f.Close()
+
+				defer func() {
+					if err := f.Close(); err != nil {
+						panic(err)
+					}
+				}()
 				reader = f
 			}
 
@@ -832,7 +845,7 @@ func calcPgMappingsToUndoBackfill(excludeBackfilling, source, target bool, exclu
 								continue
 							}
 
-							if !(included(up[i]) || included(acting[i])) {
+							if !included(up[i]) && !included(acting[i]) {
 								continue
 							}
 						} else {
@@ -842,7 +855,7 @@ func calcPgMappingsToUndoBackfill(excludeBackfilling, source, target bool, exclu
 								continue
 							}
 
-							if !(source && included(up[i]) || target && included(acting[i])) {
+							if (!source || !included(up[i])) && (!target || !included(acting[i])) {
 								continue
 							}
 						}
